7b4822b9abc34ff8742f9b8271ebfabb5337cee6
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-04-25 14:53:35 -0700

    latest DTI code working :)

--- src/dbspace/control/DTI.py
+++ src/dbspace/control/DTI.py
@@ -7,60 +7,58 @@ Created on Sun Jul  1 20:06:36 2018
 Tractography Class to preprocess and package DTI data relevant to project
 """
 
-import numpy as np
+import json
+from tkinter import W
+import tqdm
+
+import dbspace as dbo
+import matplotlib.pyplot as plt
 import nibabel
 import nilearn
 import nilearn.image as image
-from nilearn import plotting, image
-import matplotlib.pyplot as plt
-from nilearn import datasets
-from nilearn import surface
-
-from mpl_toolkits.mplot3d import Axes3D
-
-import dbspace as dbo
-from dbspace import nestdict
+import numpy as np
+from dbspace.utils.structures import nestdict
+from dbspace.utils.functions import unity
+from nilearn import image, plotting
+import copy
+import itertools
 
 
-Etrode_map = {
-    "OnT": {
-        "901": (2, 1),
-        "903": (2, 2),
-        "905": (2, 1),
-        "906": (2, 2),
-        "907": (1, 1),
-        "908": (2, 1),
-    },
-    "OffT": {
-        "901": (1, 2),
-        "903": (1, 1),
-        "905": (1, 2),
-        "906": (1, 1),
-        "907": (2, 2),
-        "908": (1, 2),
-    },
-}
-chirp_lib = {"OnT": {"901": {"BL": ()}}}
-
-
-class DTI:
-    def __init__(self, do_pts=["901", "903", "905", "906", "907", "908"]):
+class engaged_tractography:
+    def __init__(
+        self,
+        target_electrode_map,
+        do_pts=["901", "903", "905", "906", "907", "908"],
+        v_list=range(2, 8),
+        do_condits=["OnT", "OffT"],
+        base_data_dir=None,
+    ):
         self.do_pts = do_pts
-        self.v_list = [2, 3, 4, 5, 6, 7]
+        self.v_list = list(v_list)
+        self.do_condits = do_condits
+        self.stim_configurations = ["L", "R"]
+        self.base_data_dir = base_data_dir
 
-    def load_data(self):
-        dti_file = nestdict()
-        data = nestdict()
-        data_arr = np.zeros((1, 1, 6, 2, 182, 218, 182))
-        combined = nestdict()
+        self.load_electrode_map(target_electrode_map)
 
+    def load_dti(self, hide_progress=False):
+        data_arr = nestdict()
+        base_data_dir = self.base_data_dir
+
+        if hide_progress:
+            progress_fn = unity
+        else:
+            progress_fn = tqdm.tqdm
         for pp, pt in enumerate(self.do_pts):
-            for cc, condit in enumerate(["OnT"]):
-                for vv, vstim in enumerate(self.v_list):
-                    for ss, side in enumerate(["L", "R"]):
+            print(pt)
+            for cc, condit in enumerate(self.do_condits):
+                for vv, vstim in progress_fn(enumerate(self.v_list)):
+                    dti_file = {key: [] for key in self.stim_configurations}
+                    for ss, side in enumerate(self.stim_configurations):
                         cntct = dbo.Etrode_map[condit][pt][ss] + 1
                         fname = (
-                            "/home/virati/Dropbox/projects/Research/MDD-DBS/Data/Anatomy/DTI/MDT_DBS_2_7V_Tractography/DBS"
+                            base_data_dir
+                            + "MDT_DBS_2_7V_Tractography/DBS"
                             + str(pt)
                             + "."
                             + side
@@ -69,35 +67,36 @@ class DTI:
                             + str(vstim)
                             + "V.bin.nii.gz"
                         )
-                        dti_file[pt][condit][vstim][side] = fname
+                        dti_file[side] = fname
 
-                        # data[pt][condit][vstim][side] = image.smooth_img(dti_file[pt][condit][vstim][side],fwhm=1)
-                        data[pt][condit][vstim][side] = image.load_img(
-                            dti_file[pt][condit][vstim][side]
-                        )
+                    dti_data = {
+                        side: image.load_img(dti_file[side])
+                        for side in self.stim_configurations
+                    }
 
-                        data_arr[pp, cc, vv, ss, :, :, :] = np.array(
-                            data[pt][condit][vstim][side].dataobj
-                        )
-                    combined[pt][condit][vstim] = image.math_img(
+                    bilateral_dti_data_at_v = image.math_img(
                         "img1+img2",
-                        img1=data[pt][condit][vstim]["L"],
-                        img2=data[pt][condit][vstim]["R"],
+                        img1=dti_data[self.stim_configurations[0]],
+                        img2=dti_data[self.stim_configurations[1]],
                     )
+                    if vstim != 2:
+                        data_arr[pt][condit] = image.math_img(
+                            "img1+img2",
+                            img1=data_arr[pt][condit],
+                            img2=bilateral_dti_data_at_v,
+                        )
+                    else:
+                        data_arr[pt][condit] = copy.copy(bilateral_dti_data_at_v)
 
-                stim_mask = np.sum(data_arr, axis=3).squeeze()
+        self.dti_data = data_arr
 
-                middle = (stim_mask > 0).astype(np.int)
-                middle_idx = np.argmax(middle, axis=0)
-
-        self.combined = combined
-        self.stim_mask = stim_mask
-        self.middle_idx = middle_idx
-        self.data = data
+    def load_electrode_map(self, target_map_config):
+        with open(target_map_config, "r") as electrode_map:
+            self.electrode_map = json.load(electrode_map)
 
     def plot_V_thresh(self, pt="906", condit="OnT"):
-        # %%
-        # plot the
+        vstim = 2
+
         new_img = nilearn.image.new_img_like(
             self.data[pt][condit][vstim]["L"], (self.middle_idx)
         )
@@ -107,35 +106,55 @@ class DTI:
     This method plots the DTI for a given patient x condition combination
     """
 
-    def plot_V_DTI(self, pt="906", condit="OnT", v_select=2, merged=False):
-        combined = self.combined
-        vidx = self.v_list.index(v_select)
-
-        condit = "OnT"
-
-        if merged:
-            stacked = image.math_img(
-                "img1+img2+img3+img4+img5+img6",
-                img1=combined[pt][condit][2],
-                img2=combined[pt][condit][3],
-                img3=combined[pt][condit][4],
-                img4=combined[pt][condit][5],
-                img5=combined[pt][condit][6],
-                img6=combined[pt][condit][7],
-            )
+    def plot_engaged_tractography(self, condits=["OnT", "OffT"]):
+        for cc, condit in enumerate(condits):
+            engaged_tracto = self.get_engaged_tractography(condit=condit)
 
             plotting.plot_glass_brain(
-                stacked,
+                engaged_tracto,
                 black_bg=True,
                 title=condit + " Tractography",
                 vmin=-15,
                 vmax=15,
             )
-        else:
-            new_img = nilearn.image.new_img_like(
-                self.data[pt][condit][v_select]["L"], (self.middle_idx)
+
+    def get_engaged_tractography(self, condit):
+        dti_data = self.dti_data
+
+        avg_image = [dti_data[pt][condit] for pt in self.do_pts]
+
+        keys = [f"img{n}" for n, pt in enumerate(self.do_pts)]
+        # sum_string = "+".join(keys)
+        mean_string = "np.mean(np.array([" + ",".join(keys) + "]),axis=0)"
+        sum_args = {f"img{n}": dti_data[pt][condit] for n, pt in enumerate(self.do_pts)}
+
+        return image.math_img(mean_string, **sum_args)
+
+    def plot_preference_mask(self, condits=["OnT", "OffT"], threshold=0.05):
+        if len(condits) != 2:
+            raise ValueError("Preference Mask needs two conditions to compare...")
+
+        diff_map = nestdict()
+        dti_data = {key: self.get_engaged_tractography(condit=key) for key in condits}
+        for ccs in itertools.product(condits, repeat=2):
+            if ccs[0] == ccs[1]:
+                continue
+            diff_map[ccs[0]] = image.math_img(
+                "img1 > img2+" + str(threshold),
+                img1=dti_data[ccs[0]],
+                img2=dti_data[ccs[1]],
             )
-            plotting.plot_glass_brain(new_img)
+
+        for target in condits:
+            plotting.plot_glass_brain(
+                diff_map[target],
+                black_bg=True,
+                title=target + " Engaged Preference Mask",
+                vmin=-1,
+                vmax=1,
+            )
+
+    plt.show()
 
 
 if __name__ == "__main__":
